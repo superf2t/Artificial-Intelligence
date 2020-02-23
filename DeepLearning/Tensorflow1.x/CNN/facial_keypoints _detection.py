27761a68b0f185f87a3132689d53af45e4c849c7
webzhuce07 <lajiwebzhuce@163.com>
2020-02-23 15:46:05 +0800

    update Facial kepoints

--- DeepLearning/Tensorflow1.x/CNN/facial_keypoints _detection.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-
-train_df = pd.read_csv("training.csv")
