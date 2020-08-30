815da86ef08b2613142da1645cb9693b1fa10273
Mr.Bin <lajiwebzhuce@163.com>
2020-08-30 17:59:41 +0800

    update

---
+++ MachineLearning/Practice/GiveMeSomeCredit/EDA.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
--- MachineLearning/Practice/Titanic/ModelSelection.py
+++ MachineLearning/Practice/Titanic/ModelSelection.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import  cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import learning_curve
 import pandas as pd
 import warnings
@@ -7,6 +7,8 @@ warnings.filterwarnings('ignore')
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['SimHei']
+import xgboost as xgb
+from sklearn.model_selection import RandomizedSearchCV
 
 train = pd.read_csv("train0.csv")
 test = pd.read_csv("test0.csv")
@@ -35,7 +37,7 @@ result.to_csv("ressult.csv", index=False)
 
 train_sizes = np.linspace(.05, 1, 20)
 train_sizes, train_scores, test_scores = learning_curve(
-    clf, X, y,cv=None, n_jobs=1, train_sizes=train_sizes
+    clf, X, y, cv=None, n_jobs=1, train_sizes=train_sizes
 )
 train_scores_mean = np.mean(train_scores, axis=1)
 train_scores_std = np.std(train_scores, axis=1)
@@ -54,3 +56,15 @@ plt.plot(train_sizes, train_scores_mean, 'o-', color="b", label=u"训练集上�
 plt.plot(train_sizes, test_scores_mean, 'o-', color="r", label=u"交叉验证集上得分")
 plt.legend(loc="best")
 plt.show()
+
+param_gird = {
+    'max_depth': range(3, 10, 2),
+    'min_child_weight': range(1, 10, 2),
+    'gamma': [i / 10.0 for i in range(0, 5)]
+}
+model = xgb.XGBClassifier(learning_rate=0.1, n_estimators=200)
+search = RandomizedSearchCV(estimator=model, param_distributions=param_gird, cv=5)
+search.fit(X, y)
+print('CV Results: ', search.cv_results_)
+print('Best Params: ', search.best_params_)
+print('Best Score: ', search.best_score_)
