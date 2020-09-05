1a29e1d58da41bfb9aa6510c3d3ce1a88efde18c
Mr.Bin <lajiwebzhuce@163.com>
2020-09-05 16:10:08 +0800

    upate Boston Housing

---
+++ MachineLearning/NLP/main.py
@@ -0,0 +1,7 @@
+import jieba
+from gensim.models.word2vec import Word2Vec
+seg_str = "好好学习，天天向上。"
+
+print("/".join(jieba.lcut(seg_str)))    # 精简模式，返回一个列表类型的结果
+print("/".join(jieba.lcut(seg_str, cut_all=True)))      # 全模式，使用 'cut_all=True' 指定
+print("/".join(jieba.lcut_for_search(seg_str)))     # 搜索引擎模式
